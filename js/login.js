4805a255a0689e226234c1bef7f7bc41c38ae77d
IngridFerreira94 <ingrid.lohayne@hotmail.com>
2024-12-11 23:27:58 -0300

    add: project
    Adicionando tela solicitada pela atividade

---
+++ js/login.js
@@ -0,0 +1,13 @@
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById('password');
+    const passwordIcon = document.getElementById('password-icon');
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        passwordIcon.classList.remove('bi-eye');
+        passwordIcon.classList.add('bi-eye-slash');
+    } else {
+        passwordInput.type = 'password';
+        passwordIcon.classList.remove('bi-eye-slash');
+        passwordIcon.classList.add('bi-eye');
+    }
+}
